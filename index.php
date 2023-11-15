6b95a031b60266f9a78e53c6ea0b8218c88055d4
Reyadul Islam <reyadul.islam792@gmail.com>
2023-11-15 23:48:22 +0600

    m

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <!-- <h1 class="bg-secondary">Housing State Project</h1> -->
     <?php require('./parts/preloader.php'); ?>
     <?php require('./parts/header.php'); ?>
+    <?php require('./chunks/main-banner.php'); ?>
     <?php require('./body/first-section.php'); ?>
     <?php require('./body/second-section.php'); ?>
+    
 </body>
 </html>
